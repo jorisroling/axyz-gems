c1b0adfc408198f4df9081aee333394b341480a1
Joris Röling <joris@bonboa.com>
2019-02-12 01:20:28 +0100

    Ableton Control Script

---
+++ Ableton/Axyz_Gems/__init__.py
@@ -0,0 +1,20 @@
+# Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/BCR2000/__init__.py
+# Compiled at: 2018-04-23 20:27:04
+from __future__ import absolute_import, print_function, unicode_literals
+from _Generic.GenericScript import GenericScript
+import Live
+from .config import *
+
+def create_instance(c_instance):
+    u""" The generic script can be customised by using parameters (see config.py). """
+    return GenericScript(c_instance, Live.MidiMap.MapMode.absolute, Live.MidiMap.MapMode.absolute, DEVICE_CONTROLS, TRANSPORT_CONTROLS, VOLUME_CONTROLS, TRACKARM_CONTROLS, BANK_CONTROLS, CONTROLLER_DESCRIPTION)
+
+
+from _Framework.Capabilities import *
+
+def get_capabilities():
+    return {CONTROLLER_ID_KEY: controller_id(vendor_id=5015, product_ids=[
+                         188], model_name='Axyz Gems'),
+       PORTS_KEY: [
+                 inport(props=[NOTES_CC, SCRIPT]),
+                 outport(props=[NOTES_CC, SCRIPT])]}
---
+++ Ableton/Axyz_Gems/config.py
@@ -0,0 +1,25 @@
+# Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/BCR2000/config.py
+# Compiled at: 2018-04-23 20:27:04
+from __future__ import absolute_import, print_function, unicode_literals
+from .consts import *
+TRANSPORT_CONTROLS = {'STOP': -1,
+   'PLAY': -1, 'REC': -1, 'LOOP': -1,
+   'RWD': -1, 'FFWD': -1}
+DEVICE_CONTROLS = (
+ GENERIC_ENC1, GENERIC_ENC2, GENERIC_ENC3, GENERIC_ENC4,
+ GENERIC_ENC5, GENERIC_ENC6, GENERIC_ENC7, GENERIC_ENC8)
+VOLUME_CONTROLS = (
+ (-1, -1), (-1, -1), (-1, -1),
+ (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1))
+TRACKARM_CONTROLS = (-1, -1, -1, -1, -1, -1, -1, -1)
+BANK_CONTROLS = {'TOGGLELOCK': -1,
+   'BANKDIAL': -1,
+   'NEXTBANK': -1,
+   'PREVBANK': -1, 'BANK1': -1,
+   'BANK2': -1, 'BANK3': -1, 'BANK4': -1, 'BANK5': -1,
+   'BANK6': -1, 'BANK7': -1, 'BANK8': -1}
+CONTROLLER_DESCRIPTION = {'INPUTPORT': 'LIGHTPAD BLOCK', 'OUTPUTPORT': 'LIGHTPAD BLOCK', 'CHANNEL': 0}
+MIXER_OPTIONS = {'NUMSENDS': 2,
+   'SEND1': (-1, -1, -1, -1, -1, -1, -1, -1),
+   'SEND2': (-1, -1, -1, -1, -1, -1, -1, -1),
+   'MASTERVOLUME': -1}
